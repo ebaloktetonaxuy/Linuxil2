1b415544a256cf4cffd154372dea5b481c6a5224
AmoreForever <103936585+AmoreForever@users.noreply.github.com>
2022-05-30 21:09:06 +0500

    Update main.py

--- friendly-telegram/main.py
+++ friendly-telegram/main.py
@@ -52,7 +52,7 @@ from .database import backend, frontend
 from .dispatcher import CommandDispatcher
 from .translations.core import Translator
 
-__version__ = (3, 1, 25)
+__version__ = (1, 1, 0)
 is_okteto = "OKTETO" in os.environ
 
 BASE_DIR = "/data" if is_okteto else os.path.dirname(utils.get_base_dir())
